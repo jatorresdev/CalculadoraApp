23188f3ff8cf92f2011163035eda3cd48595c413
Angel Torres <jatorresdev@gmail.com>
2016-05-19 23:41:19 -0500

    Se añade funcionalidad para cada uno de los botones de la calculadora.

--- app/src/main/java/com/example/jatorresdev/calculadora/MainActivity.java
+++ app/src/main/java/com/example/jatorresdev/calculadora/MainActivity.java
@@ -5,11 +5,117 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    private String operation;
+    private Double num1, num2, result;
 
     public void onClickOne(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "1");
+    }
+
+    public void onClickTwo(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "2");
+    }
+
+    public void onClickThree(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "3");
+    }
+
+    public void onClickFour(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "4");
+    }
+
+    public void onClickFive(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "5");
+    }
+
+    public void onClickSix(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "6");
+    }
+
+    public void onClickSeven(View myView) {
         EditText display = (EditText) findViewById(R.id.txt_display);
-        display.setText(display.getText() + "1");
+        display.setText(display.getText() + "7");
+    }
+
+    public void onClickEight(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "8");
+    }
+
+    public void onClickNine(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "9");
+    }
+
+    public void onClickZero(View myView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText(et.getText() + "0");
+    }
+
+    public void onClickSum(View myView) {
+        operation = "+";
+        onClickOperatorNum1(myView);
+    }
+
+    public void onClickSub(View myView) {
+        operation = "-";
+        onClickOperatorNum1(myView);
+    }
+
+    public void onClickMultiply(View myView) {
+        operation = "*";
+        onClickOperatorNum1(myView);
+    }
+
+    public void onClickDivision(View myView) {
+        operation = "/";
+        onClickOperatorNum1(myView);
+    }
+
+    public void onClickOperatorNum1(View miView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        num1 = Double.parseDouble(et.getText().toString());
+        et.setText("");
+    }
+
+    public void onClickEqual(View miView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        num2 = Double.parseDouble(et.getText().toString());
+
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+
+        DecimalFormat df = new DecimalFormat("###.#");
+        et.setText(df.format(result).toString());
+    }
+
+    public void onClickClear(View miView) {
+        EditText et = (EditText) findViewById(R.id.txt_display);
+        et.setText("");
+
+        num1 = 0.0;
+        num2 = 0.0;
     }
 
     @Override
